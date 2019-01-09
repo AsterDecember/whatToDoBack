e10966aa2fa95dbfd7d1e5da16c466921e0121fd
AsterDecember <aster.december@gmail.com>
2019-01-09 11:54:18 -0600

    auth basic endpoints working with passport

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors')
+const passport     = require('./middleware/passport')
+const session      = require('express-session')
+const MongoStore   = require('connect-mongo')(session)
 
 
+// Mongoose configuration
 mongoose
-  .connect('mongodb://localhost/back', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+    .connect(process.env.DB, {useNewUrlParser: true})
+    .then(x => {
+      //console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch(err => {
+      console.error('Error connecting to mongo', err)
+    });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -30,6 +35,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// session in order to save the user logged
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection:mongoose.connection,
+    ttl:24*60*60
+  }),
+  secret: 'mikez',
+  resave: true,
+  saveUninitialized:true,
+  cookie:{httpOnly:true,maxAge:60000}
+}))
+// passport initialized
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -37,7 +57,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -47,12 +67,18 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Auth Mike Api';
 
 
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3001']
+}))
 
 const index = require('./routes/index');
 app.use('/', index);
+const authRoutes = require('./routes/auth')
+app.use('/api',authRoutes)
 
 
 module.exports = app;
