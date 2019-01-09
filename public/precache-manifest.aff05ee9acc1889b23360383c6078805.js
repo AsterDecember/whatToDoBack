27bf42806a278610a7b51826f2a3c84547d08822
AsterDecember <aster.december@gmail.com>
2019-01-09 16:40:35 -0600

    add front to public

---
+++ public/precache-manifest.aff05ee9acc1889b23360383c6078805.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90446450e7ce519809221d6b08913b4a",
+    "url": "/static/media/banner.90446450.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23ba95e1d9c47dd05ed2",
+    "url": "/static/js/main.23ba95e1.chunk.js"
+  },
+  {
+    "revision": "bad18f2764a38894ab01",
+    "url": "/static/js/1.bad18f27.chunk.js"
+  },
+  {
+    "revision": "23ba95e1d9c47dd05ed2",
+    "url": "/static/css/main.94de02dd.chunk.css"
+  },
+  {
+    "revision": "bad18f2764a38894ab01",
+    "url": "/static/css/1.8fb90d35.chunk.css"
+  },
+  {
+    "revision": "c087672cdc836577056629e0332a3a51",
+    "url": "/index.html"
+  }
+];
